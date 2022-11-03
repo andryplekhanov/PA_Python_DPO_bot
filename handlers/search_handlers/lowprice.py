5f8ac277e0a7c0fd249086baa4c9552902f0ad49
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-03 20:18:32 +0300

    в lowprice внесены улучшения

--- handlers/search_handlers/lowprice.py
+++ handlers/search_handlers/lowprice.py
@@ -1,6 +1,6 @@
 from telebot.types import Message
-from keyboards.reply.yes_no_reply import get_yes_no
-from keyboards.inline.calendar import print_calendar
+# from keyboards.reply.yes_no_reply import get_yes_no
+from keyboards.inline.yes_no_reply import get_yes_no
 from datetime import date, timedelta
 from loader import bot
 from states.search_info import LowPriceStates
@@ -31,7 +31,6 @@ def get_amount_hotels(message: Message):
     if 1 <= int(message.text) <= 10:
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['amount_hotels'] = int(message.text)
-        bot.set_state(message.from_user.id, LowPriceStates.need_photo, message.chat.id)
         bot.send_message(message.from_user.id, 'Желаете загрузить фото отелей?', reply_markup=get_yes_no())
     else:
         bot.send_message(message.from_user.id, 'Количество отелей в топе должно быть от 1 до 10')
@@ -42,20 +41,19 @@ def amount_hotels_incorrect(message: Message):
     bot.send_message(message.from_user.id, 'Введите число от 1 до 10')
 
 
-@bot.message_handler(state=LowPriceStates.need_photo)
-def get_need_photo(message: Message):
-    if message.text == 'Да':
-        bot.send_message(message.from_user.id, text='Введите количество фото')
-        bot.set_state(message.from_user.id, LowPriceStates.amount_photo, message.chat.id)
-        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+@bot.callback_query_handler(func=lambda call: call.data == 'yes' or call.data == 'no')
+def need_photo_reply(call):
+    with bot.retrieve_data(call.message.chat.id, call.message.chat.id) as data:
+        if call.data == "yes":
+            bot.send_message(call.message.chat.id, text='Введите количество фото')
             data['need_photo'] = True
-    elif message.text == 'Нет':
-        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            bot.set_state(call.message.chat.id, LowPriceStates.amount_photo, call.message.chat.id)
+        elif call.data == "no":
             data['need_photo'] = False
             data['amount_photo'] = 0
-        ask_for_start_date(message)
-    else:
-        bot.send_message(message.from_user.id, text='Напишите "Да" или "Нет"')
+            ask_for_start_date(call.message)
+        else:
+            bot.send_message(call.message.from_user.id, text='Нажмите кнопку "Да" или "Нет"')
 
 
 @bot.message_handler(state=LowPriceStates.amount_photo, is_digit=True)  # Если количество фото - число
@@ -84,7 +82,8 @@ def date_reply(call):
         if not data.get('start_date'):
             result, key, step = DetailedTelegramCalendar(min_date=date.today()).process(call.data)
         elif not data.get('end_date'):
-            result, key, step = DetailedTelegramCalendar(min_date=data.get('start_date') + timedelta(1)).process(call.data)
+            new_start_date = data.get('start_date') + timedelta(1)
+            result, key, step = DetailedTelegramCalendar(min_date=new_start_date).process(call.data)
 
     if not result and key:
         bot.edit_message_text(f"Введите дату", call.message.chat.id,
@@ -97,7 +96,6 @@ def date_reply(call):
             elif not data.get('end_date'):
                 data['end_date'] = result
                 data_dict = data
-                bot.delete_state(call.message.from_user.id, call.message.chat.id)
                 ready_for_answer(call.message, data_dict)
 
 
@@ -107,6 +105,7 @@ def ask_for_end_date(message, start_date):
 
 
 def ready_for_answer(message, data):
+    bot.delete_state(message.from_user.id, message.chat.id)
     amount_nights = int((data['end_date'] - data['start_date']).total_seconds() / 86400)
     reply_str = f"Ок, ищем: <b>топ {data['amount_hotels']}</b> " \
                 f"самых дешёвых отелей в городе <b>{data['city'].capitalize()}</b>\n" \
