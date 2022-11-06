2a82837077e98b3f27ae53efcc98a6069a39f1a9
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-06 12:53:33 +0300

    исправлены описания команд

--- config_data/config.py
+++ config_data/config.py
@@ -11,10 +11,10 @@ RAPID_API_KEY = os.getenv('RAPID_API_KEY')
 DEFAULT_COMMANDS = (
     ('start', "Запустить бота"),
     ('help', "Вывести справку"),
-    ('lowprice', "Вывести топ самых дешёвых отелей в городе"),
-    ('highprice', "Вывести топ самых дорогих отелей в городе"),
+    ('lowprice', "Топ самых дешёвых отелей в городе"),
+    ('highprice', "Топ самых дорогих отелей в городе"),
     ('bestdeal', "Настраиваемый поиск отелей"),
-    ('history', "Вывести историю поиска")
+    ('history', "История поиска")
 )
 RAPID_API_URL = "https://hotels4.p.rapidapi.com/v2/get-meta-data"
 RAPID_API_HEADERS = {
