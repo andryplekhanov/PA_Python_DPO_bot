9d7a7f59e091b49cc7f0efc39174a4f963456c56
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-06 17:37:30 +0300

    парсер и обработчик фото

---
+++ utils/get_photos.py
@@ -0,0 +1,25 @@
+from utils.api_reqiest import request_to_api
+from config_data.config import RAPID_API_HEADERS, RAPID_API_ENDPOINTS
+import json
+
+
+def parse_photos(hotel_id):
+    querystring = {"id": hotel_id}
+    responce = request_to_api(
+        url=RAPID_API_ENDPOINTS['hotel-photos'],
+        querystring=querystring,
+        headers=RAPID_API_HEADERS)
+    if responce:
+        result = json.loads(responce.text).get('hotelImages')
+        return result
+    return None
+
+
+def process_photos(all_photos, amount_photos):
+    photos = all_photos[:amount_photos]
+    result = list()
+    for photo in photos:
+        url = photo.get('baseUrl')
+        suffix = [size.get('suffix') for size in photo.get('sizes') if size.get('suffix') in ('z', 'w')][0]
+        result.append(url.format(size=suffix))
+    return result
