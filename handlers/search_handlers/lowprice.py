9a7d3839ad017faaefbb39655bab87e048ea294c
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-07 15:58:46 +0300

    прописал условия при ошибках в парсинге и обработке данных

--- handlers/search_handlers/lowprice.py
+++ handlers/search_handlers/lowprice.py
@@ -124,30 +124,34 @@ def ready_for_answer(message, data):
                 f" — <b>{data['amount_photo']}</b> штук\n" \
                 f"Длительность поездки: <b>{amount_nights} ноч.</b> " \
                 f"(с {data['start_date']} по {data['end_date']})."
-    bot.send_message(message.chat.id, reply_str, parse_mode="html")
+    bot.edit_message_text(reply_str, message.chat.id, message.message_id, parse_mode="html")
 
-    hotels = parse_hotels(data)['results']
-
+    hotels = parse_hotels(data).get('results')
     if hotels:
         result_dict = process_hotels_info(hotels, amount_nights)
-        for hotel_id, hotel_data in result_dict.items():
-            hotel_info_str = get_hotel_info_str(hotel_data, amount_nights)
-            bot.send_message(message.chat.id, hotel_info_str, parse_mode="html", disable_web_page_preview=True)
+        if result_dict:
+            for hotel_id, hotel_data in result_dict.items():
+                hotel_info_str = get_hotel_info_str(hotel_data, amount_nights)
+                bot.send_message(message.chat.id, hotel_info_str, parse_mode="html", disable_web_page_preview=True)
 
-            if data['need_photo']:
-                all_photos = "https://www.hotels.com/ho{id}/?pwaThumbnailDialog=thumbnail-gallery".format(id=hotel_id)
-                msg = "<b>🖼 Фото отеля:</b>\n" \
-                      "   больше фото <a href='{all_photos}'>по ссылке >></a>".format(all_photos=all_photos)
-                bot.send_message(message.chat.id, msg, parse_mode="html", disable_web_page_preview=True)
-                photos_info_list = parse_photos(hotel_id)
-                if photos_info_list:
-                    photos_list = process_photos(photos_info_list, data['amount_photo'])
-                    if photos_list:
-                        for photo_url in photos_list:
-                            bot.send_photo(message.chat.id, photo_url)
+                if data['need_photo']:
+                    all_photos = "https://www.hotels.com/ho{id}/?pwaThumbnailDialog=thumbnail-gallery".format(
+                        id=hotel_id)
+                    msg = "<b>🖼 Фото отеля:</b>\n" \
+                          "    больше фото <a href='{all_photos}'>по ссылке >></a>".format(all_photos=all_photos)
+                    bot.send_message(message.chat.id, msg, parse_mode="html", disable_web_page_preview=True)
+
+                    photos_info_list = parse_photos(hotel_id)
+                    if photos_info_list:
+                        photos_list = process_photos(photos_info_list, data['amount_photo'])
+                        if photos_list:
+                            for photo_url in photos_list:
+                                bot.send_photo(message.chat.id, photo_url)
+                        else:
+                            bot.send_message(message.chat.id, '⚠️ Ошибка загрузки фото.')
                     else:
-                        bot.send_message(message.chat.id, '⚠️ Ошибка загрузки фото.')
-                else:
-                    bot.send_message(message.chat.id, '⚠️ Ошибка загрузки фото.')
+                        bot.send_message(message.chat.id, '⚠️ У отеля нет фото.')
+        else:
+            bot.send_message(message.chat.id, '⚠️ Не удалось загрузить информацию по отелям города!')
     else:
         bot.send_message(message.chat.id, '⚠️ Ошибка. Попробуйте ещё раз!')
