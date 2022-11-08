0fdafc9db6e36e78b5c45e625e69363229975e66
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-08 20:00:14 +0300

    добавлена команда bestdeal

--- handlers/search_handlers/bestdeal.py
+++ handlers/search_handlers/bestdeal.py
@@ -1,7 +1,13 @@
 from telebot.types import Message
 from loader import bot
+from states.search_info import UsersStates
+from handlers import survey_handlers
 
 
 @bot.message_handler(commands=['bestdeal'])
-def bot_bestdeal(message: Message):
-    bot.reply_to(message, "Топ отелей, наиболее подходящих по цене и расположению от центра")
+def bot_best_deal(message: Message):
+    bot.delete_state(message.from_user.id, message.chat.id)
+    bot.set_state(message.from_user.id, UsersStates.cities, message.chat.id)
+    bot.send_message(message.from_user.id, 'Введите город')
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['last_command'] = 'bestdeal'
