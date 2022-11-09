b3aa06271f6515c22fbc18d714605cdd5e0598d0
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-09 19:45:39 +0300

    доб. фун. вывода результата для bestdeal

--- utils/ready_for_answer.py
+++ utils/ready_for_answer.py
@@ -16,9 +16,9 @@ def low_high_price_answer(message: Message, data: Dict) -> None:
                 f"(с {data['start_date']} по {data['end_date']})."
     bot.edit_message_text(reply_str, message.chat.id, message.message_id, parse_mode="html")
 
-    hotels = parse_hotels(data).get('results')
+    hotels = parse_hotels(data)
     if hotels:
-        result_dict = process_hotels_info(hotels, amount_nights)
+        result_dict = process_hotels_info(hotels.get('results'), amount_nights)
         if result_dict:
             for hotel_id, hotel_data in result_dict.items():
                 hotel_info_str = get_hotel_info_str(hotel_data, amount_nights)
@@ -52,5 +52,37 @@ def print_photo(message: Message, hotel_id: int, amount_photo: int) -> None:
 
 
 def best_deal_answer(message: Message, data: Dict) -> None:
-    print(data)
-    return
+    amount_nights = int((data['end_date'] - data['start_date']).total_seconds() / 86400)
+    reply_str = f"✅ Ок, ищем: <b>топ {data['amount_hotels']}</b> отелей в городе <b>{data['city']}</b>\n" \
+                f"В ценовом диапазоне <b>от {data['start_price']}$ до {data['end_price']}$</b>\n" \
+                f"Максимальная удаленность от центра: <b>{data['end_distance']} Км</b>\n" \
+                f"{f'Нужно загрузить фото' if data['need_photo'] else f'Фото не нужны'}" \
+                f" — <b>{data['amount_photo']}</b> штук\n" \
+                f"Длительность поездки: <b>{amount_nights} ноч.</b> " \
+                f"(с {data['start_date']} по {data['end_date']})."
+    bot.send_message(message.chat.id, reply_str, parse_mode="html")
+
+    hotels = parse_hotels(data)
+    if hotels:
+        pre_result_dict = process_hotels_info(hotels.get('results'), amount_nights)
+        if pre_result_dict:
+            hotels_counter = 0
+            for hotel_id, hotel_data in pre_result_dict.items():
+                if hotels_counter >= data.get('amount_hotels'):
+                    break
+                current_distance = hotel_data.get('distance_city_center')
+                if not current_distance:
+                    continue
+                if current_distance <= data.get('end_distance'):
+                    hotel_info_str = get_hotel_info_str(hotel_data, amount_nights)
+                    bot.send_message(message.chat.id, hotel_info_str, parse_mode="html", disable_web_page_preview=True)
+
+                    if data['need_photo']:
+                        print_photo(message, hotel_id, data['amount_photo'])
+                    hotels_counter += 1
+            if hotels_counter == 0:
+                bot.send_message(message.chat.id, '⚠️ Ничего не нашлось! Измените критерии поиска!')
+        else:
+            bot.send_message(message.chat.id, '⚠️ По вашему запрос ничего не нашлось! Измените критерии поиска!')
+    else:
+        bot.send_message(message.chat.id, '⚠️ Ошибка. Ничего не нашлось!')
