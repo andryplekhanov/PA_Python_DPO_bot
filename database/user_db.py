4b497bce367c2f06c9bea8c37e03f5c51bd864e4
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-10 16:23:49 +0300

    основа бд

---
+++ database/user_db.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+
+class UserDb:
+    def __init__(self, db_file):
+        pass
---
+++ database/user_history_db.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+
+class HistoryUserDb:
+    def __init__(self, db_file):
+        pass
