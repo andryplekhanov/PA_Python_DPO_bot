a6bfa83a3424186adfc98c0ea80ac7e919bb7b30
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-10 16:21:27 +0300

    в загрузчик прописал базы данных

--- loader.py
+++ loader.py
@@ -1,7 +1,11 @@
 from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
+from database.user_db import UserDb
+from database.user_history_db import HistoryUserDb
 
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
 
+db_user = UserDb('user.db')
+db_history = HistoryUserDb('history_user.db')
