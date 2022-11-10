3bfaf116d99d4e7bd70698919f49f2989f29c644
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-10 12:33:59 +0300

    реализован хендлер истории поиска

--- handlers/search_handlers/history.py
+++ handlers/search_handlers/history.py
@@ -1,7 +1,23 @@
-from telebot.types import Message
+from telebot.types import Message, CallbackQuery
 from loader import bot
+from keyboards.inline.history_action_choice import get_history_action
+from utils.get_history import show_history, delete_history
 
 
 @bot.message_handler(commands=['history'])
 def bot_history(message: Message):
-    bot.reply_to(message, "История поиска отелей")
+    bot.delete_state(message.from_user.id, message.chat.id)
+    bot.send_message(message.from_user.id, 'Пожалуйста, уточните:', reply_markup=get_history_action())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'show_history' or call.data == 'delete_history')
+def process_history_reply(call: CallbackQuery) -> None:
+    if call.data == "show_history":
+        bot.send_message(call.message.chat.id, text='История поиска:')
+        show_history()
+    elif call.data == "delete_history":
+        delete_history()
+        bot.send_message(call.message.chat.id, text='История поиска очищена!')
+    bot.send_message(call.message.chat.id,
+                     f"😉👌 Вот как-то так.\nМожете ввести ещё какую-нибудь команду!\n"
+                     f"Например: <b>/help</b>", parse_mode="html")
