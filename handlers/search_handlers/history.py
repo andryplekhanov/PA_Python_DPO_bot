754bbed8ae1bb94bc3c4085789e6ad5b600aea7a
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-11 20:03:49 +0300

    доб. отлов исключений

--- handlers/search_handlers/history.py
+++ handlers/search_handlers/history.py
@@ -1,7 +1,7 @@
 from telebot.types import Message, CallbackQuery
 from loader import bot
 from keyboards.inline.history_action_choice import get_history_action
-from utils.get_history import show_history, delete_history
+from database.db_controller import show_history, delete_history
 
 
 @bot.message_handler(commands=['history'])
@@ -14,10 +14,15 @@ def bot_history(message: Message):
 def process_history_reply(call: CallbackQuery) -> None:
     if call.data == "show_history":
         bot.send_message(call.message.chat.id, text='История поиска:')
-        show_history()
+        try:
+            show_history(call.message, user=call.from_user.username)
+        except Exception:
+            bot.send_message(call.message.chat.id, text='⚠️Упс... ошибка: не могу загрузить историю поиска:')
     elif call.data == "delete_history":
-        delete_history()
-        bot.send_message(call.message.chat.id, text='История поиска очищена!')
+        try:
+            delete_history(call.message, user=call.from_user.username)
+        except Exception:
+            bot.send_message(call.message.chat.id, text='⚠️Упс... ошибка: не могу удалить историю поиска:')
     bot.send_message(call.message.chat.id,
                      f"😉👌 Вот как-то так.\nМожете ввести ещё какую-нибудь команду!\n"
                      f"Например: <b>/help</b>", parse_mode="html")
