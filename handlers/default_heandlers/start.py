0268858063d114b2d26c8c0fdb5ac36c34df492c
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-11 12:07:51 +0300

    команда start: перенес создание таблиц в main

--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -1,8 +1,12 @@
 from telebot.types import Message
 from loader import bot
+from database.db_controller import save_user
 
 
 @bot.message_handler(commands=['start'])
-def bot_start(message: Message):
+def bot_start(message: Message) -> None:
+    save_user(message)
     bot.delete_state(message.from_user.id, message.chat.id)
-    bot.reply_to(message, f"Привет, {message.from_user.full_name}!")
+    bot.send_message(message.chat.id, f"👋 Привет, {message.from_user.full_name}!\n"
+                                      f"Можете ввести какую-нибудь команду!\n"
+                                      f"Например: <b>/help</b>", parse_mode="html")
