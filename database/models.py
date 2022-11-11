2eed5d013afc25a7942f42b735e04574e761d657
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-11 20:02:17 +0300

    исправлены старые классы и добавлен SearchResult

--- database/models.py
+++ database/models.py
@@ -3,25 +3,42 @@ from loader import db
 
 
 class BaseModel(Model):
-    id = PrimaryKeyField(unique=True)
-
     class Meta:
         database = db
-        order_by = 'id'
 
 
 class User(BaseModel):
-    name = CharField()
+    id = PrimaryKeyField(unique=True)
+    name = CharField(unique=True)
 
     class Meta:
         db_table = 'users'
+        order_by = 'id'
 
 
 class History(BaseModel):
     date = DateField()
     command = CharField()
-    hotels = CharField()
-    user_id = ForeignKeyField(User)
+    city = CharField()
+    start_date = DateField()
+    end_date = DateField()
+    from_user = ForeignKeyField(User.name)
 
     class Meta:
         db_table = 'histories'
+        order_by = 'date'
+
+
+class SearchResult(BaseModel):
+    hotel_id = IntegerField()
+    hotel_name = CharField()
+    price_per_night = FloatField()
+    total_price = FloatField()
+    distance_city_center = FloatField()
+    hotel_url = CharField()
+    hotel_neighbourhood = CharField()
+    from_date = ForeignKeyField(History.date)
+
+    class Meta:
+        db_table = 'results'
+        order_by = 'price_per_night'
