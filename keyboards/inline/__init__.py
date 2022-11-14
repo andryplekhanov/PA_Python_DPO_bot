9b4993ecb440f15febbb28deee58b86d2f762a94
Andrey Plekhanov <1989plekhanov@gmail.com>
2022-11-14 19:01:39 +0300

    актуализировал иниты

--- keyboards/inline/__init__.py
+++ keyboards/inline/__init__.py
@@ -3,3 +3,4 @@ from . import filters
 from . import cities_for_choice
 from . import history_result_choice
 from . import history_action_choice
+from . import paginator
--- utils/__init__.py
+++ utils/__init__.py
@@ -6,3 +6,4 @@ from . import get_hotels
 from . import get_cities
 from . import ready_for_answer
 from . import set_bot_commands
+from . import pager_for_paginator
